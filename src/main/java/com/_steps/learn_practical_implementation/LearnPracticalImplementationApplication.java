33eb8a7212fdd646c5f6385b87d1943564c42993
PoojaGupta83 <PoojaGuptaa393@gmail.com>
2026-05-13 14:28:15 +0530

    First commit

---
+++ src/main/java/com/_steps/learn_practical_implementation/LearnPracticalImplementationApplication.java
@@ -0,0 +1,13 @@
+package com._steps.learn_practical_implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnPracticalImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnPracticalImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_steps/learn_practical_implementation/LearnPracticalImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com._steps.learn_practical_implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnPracticalImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
